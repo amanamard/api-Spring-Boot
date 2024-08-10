0e22304569acc42ed34a587498650a6a27d20817
amanamard <amanamard@gmail.com>
2024-08-10 19:32:48 -0400

    Add api project update

--- api/src/main/java/com/openclassrooms/api/model/Employee.java
+++ api/src/main/java/com/openclassrooms/api/model/Employee.java
@@ -17,11 +17,11 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="first_name")
-    private String first_name;
+    @Column(name="firstName")
+    private String firstName;
     
-    @Column(name="last_name")
-    private String last_name;
+    @Column(name="lastName")
+    private String lastName;
     
     private String mail;
     
